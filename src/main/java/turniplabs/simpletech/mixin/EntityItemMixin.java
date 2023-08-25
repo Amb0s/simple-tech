98c98f55bbc721f8ba0691ab5062fcf160363ed0
Amb0s <ambos@disroot.org>
2023-08-25 20:55:37 +0200

    Added (disabled) workaround for #13

---
+++ src/main/java/turniplabs/simpletech/mixin/EntityItemMixin.java
@@ -0,0 +1,16 @@
+package turniplabs.simpletech.mixin;
+
+import net.minecraft.core.entity.EntityItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = EntityItem.class, remap = false)
+public class EntityItemMixin {
+    @Inject(method = "clumpToNearbyStack", at = @At("HEAD"),
+            require = 0, cancellable = true)
+    private void removeItemEntityStacking(CallbackInfo ci) {
+        ci.cancel();
+    }
+}
